101fca5524a46a8b6ab877e08212db91230d28fe
Anna Kaplan <annakaplanart@gmail.com>
2018-10-31 12:54:57 -0400

    28 finished edit component

--- client/components/index.js
+++ client/components/index.js
@@ -6,4 +6,5 @@
 export {default as Navbar} from './navbar'
 export {default as AdminHome} from './adminHome'
 export {default as AddProduct} from './addProduct'
+export {default as EditProduct} from './editProduct'
 export {Login, Signup} from './auth-form'
--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,7 @@ router.use('/users', require('./users'))
 router.use('/products', require('./products'))
 
 router.use((req, res, next) => {
+  console.log('here')
   const error = new Error('Not Found')
   error.status = 404
   next(error)
