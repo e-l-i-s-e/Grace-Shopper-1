8999d8f94bdd60b37068b16310a176bb31c34a32
Ay-Nur Najm <aynajm@gmail.com>
2018-10-31 16:25:18 -0400

    fix-git-merge

--- client/routes.js
+++ client/routes.js
@@ -23,6 +23,9 @@ class Routes extends Component {
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route exact path="/products" component={Main} />
+        {/* <Route path ="/categories" component={Main} /> */}
+
+
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
