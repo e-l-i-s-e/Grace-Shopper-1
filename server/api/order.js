777b52c7ebc3fd68e43c3050c385a28d1a5fa924
Anna Kaplan <annakaplanart@gmail.com>
2018-11-02 12:35:09 -0400

    feature-loggedInCart comments inserted  in api order router code

--- server/api/order.js
+++ server/api/order.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const {Order, Product, OrderProduct} = require('../db/models')
 module.exports = router
 
-
+// api route to get cart items or intialize an empty cart
 router.get('/:userId', async(req, res, next) => {
     try{
         
@@ -24,6 +24,8 @@ router.get('/:userId', async(req, res, next) => {
     }
 })
 
+// add an item to the cart - the req.body contains
+// order id, user.id , porduct id and comments
 router.post('/', async(req, res, next) => {
     try{
         await OrderProduct.create(req.body)
