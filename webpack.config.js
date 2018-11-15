001b74900fff6e5bfbc86826f761286b12db193f
Elise Bonner <elise0209@mac.com>
2018-11-15 11:03:44 -0500

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: isDev ? 'development' : 'production',
   entry: [
     '@babel/polyfill', // enables async-await
-    './client/index.js'
+    './index.js'
   ],
   output: {
     path: __dirname,
